294940995149b96db278467e309e9f4f3f2c25c6
Ammaar-Alam <ammaar@princeton.edu>
2024-06-22 18:09:58 -0400

    fixed all bugs

--- public/script.js
+++ public/script.js
@@ -41,15 +41,31 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  function updateStatus() {
+    const doorSwitch = document.getElementById("doorSwitch");
+    const openStatus = document.getElementById("open");
+    const closedStatus = document.getElementById("closed");
+
+    if (doorSwitch.checked) {
+      closedStatus.style.color = "rgba(255, 94, 85, 1)"; // Solid Red
+      openStatus.style.color = "#888"; // Default gray
+    } else {
+      openStatus.style.color = "rgba(76, 175, 80, 1)"; // Solid Green
+      closedStatus.style.color = "#888"; // Default gray
+    }
+  }
+
   function toggleSwitch() {
     const doorSwitch = document.getElementById("doorSwitch");
     const command = doorSwitch.checked ? "open" : "close";
     sendCommand(command);
+    updateStatus();
   }
 
   const doorSwitch = document.getElementById("doorSwitch");
   if (doorSwitch) {
     doorSwitch.onclick = toggleSwitch;
+    updateStatus(); // Initialize the status on page load
   } else {
     console.error("doorSwitch element not found");
   }
